0914c77086905efee52f076fcd78ed576ecd7698
“itsMohammedNayeem” <Mohammed.Nayeem@weareplanet.com>
2023-11-14 10:28:42 +0530

    first commit

---
+++ src/context/globalContext.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+export const GlobalContext = React.createContext({
+  currentTheme: "",
+  themeSwitchHandler: () => {},
+});
+
+const GlobalContextProvider = (props) => {
+  const [currentTheme, setCurrentTheme] = useState(
+    window.localStorage.getItem("theme") == null
+      ? "light"
+      : window.localStorage.getItem("theme")
+  );
+
+  const themeSwitchHandler = (themeType) => {
+    setCurrentTheme(themeType);
+  };
+
+  // save theme to local storage
+  return (
+    <GlobalContext.Provider
+      value={{
+        theme: currentTheme,
+        themeSwitchHandler: themeSwitchHandler,
+      }}
+    >
+      {props.children}
+    </GlobalContext.Provider>
+  );
+};
+
+export default GlobalContextProvider;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+import GlobalContextProvider from "./context/globalContext";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <GlobalContextProvider>
+      <App />
+    </GlobalContextProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
